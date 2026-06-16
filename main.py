3f8ea476a678261ca68e1e9a914d2a01fd38b3ea
ozntrz11111 <ozntrz11111@gmail.com>
2026-06-16 04:40:04 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-06-16T01:39:52.063Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-06-16T01:40:06.497Z
+Commit: Fix broken links in documentation
