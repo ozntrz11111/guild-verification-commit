123799b6f43fc1367b0d85a9980ad11c4b834edb
ozntrz11111 <ozntrz11111@gmail.com>
2026-06-16 04:34:31 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-06-16T01:34:07.516Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-06-16T01:34:33.451Z
+Commit: Resolve null pointer exception in user service
