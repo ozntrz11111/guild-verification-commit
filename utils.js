ec902109f08750d1ed26dc27ceae5c4b8ac30bb6
ozntrz11111 <ozntrz11111@gmail.com>
2026-06-16 04:35:31 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-06-16T01:35:08.089Z
-Commit: Add export functionality for reports
+Timestamp: 2026-06-16T01:35:33.350Z
+Commit: Patch API endpoint error handling
