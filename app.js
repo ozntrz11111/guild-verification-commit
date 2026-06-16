282d95db9dc77d83c5ee5890f0d584a2f41a53c6
ozntrz11111 <ozntrz11111@gmail.com>
2026-06-16 04:40:19 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-06-16T01:40:04.913Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-06-16T01:40:21.626Z
+Commit: Fix broken links in documentation
