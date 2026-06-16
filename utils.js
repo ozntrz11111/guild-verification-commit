2f253f2768400e93c7978c12f89b6702ef3f38e5
ozntrz11111 <ozntrz11111@gmail.com>
2026-06-16 04:35:55 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-06-16T01:35:33.350Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-06-16T01:35:57.665Z
+Commit: Resolve null pointer exception in user service
